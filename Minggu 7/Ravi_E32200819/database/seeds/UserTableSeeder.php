3a33b0cebd6dc3c0dce88d2bfbfd686e373698cf
ravi tri j <ravijulianto46@gmail.com>
2021-04-15 10:44:43 +0700

    tugas Minggu 7

---
+++ Minggu 7/Ravi_E32200819/database/seeds/UserTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt(12345678)
+        ]);
+    }
+}
